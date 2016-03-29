96aaafbcd55f1133471fd5879cabf16265e09739
luislado <luis.r.lado@gmail.com>
2016-03-29 12:02:03 +0200

    Create velutina.R
    Script to model the distribution of Vespa velutina from field observations and auxiliary rasters

---
+++ velutina.R
@@ -0,0 +1,7 @@
+# Script to model the distribution of Vespa velutina
+# From field observations and auxiliary rasters
+# Created by Luis Rodriguez-Lado on March 29th 2016
+# at Santiago de Compostela, Spain
+
+library(raster)
+
