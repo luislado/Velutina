88cc5b6437f13f3955106cfc68b2beab00226a68
luislado <luis.r.lado@gmail.com>
2016-03-29 12:48:12 +0200

    Update velutina.R

--- velutina.R
+++ velutina.R
@@ -3,5 +3,15 @@
 # Created by Luis Rodriguez-Lado on March 29th 2016
 # at Santiago de Compostela, Spain
 
-library(raster)
+# Load libraries
+  library(foreign)
+  library(sp)
+  library(rgdal)
+  library(raster)
+  setwd("/Users/luislado/Dropbox/CSIC/RUSLE/Estaciones"); 
+
+# Retrieve auxiliary rasters
+  T_an <- raster("/Volumes/LACIE SHARE/Atlas/Proxies/Clima_aire/Temperatura/T_00_06.tif")
+  P_an <- raster("/Volumes/LACIE SHARE/Atlas/Proxies/Clima_aire/Precipitacion/P_00_06.tif")
+ 
 
